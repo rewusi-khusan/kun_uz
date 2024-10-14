844a4d6b72746937b6774078f7378d226117e245
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2024-10-14 17:04:10 +0500

    commit

--- news/views.py
+++ news/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
-<<<<<<< HEAD
-def index(request):
-    return render(request, 'index.html')
-=======
+from news.models import News
 
-def new (request):
-    return "new"
->>>>>>> 46af1ce5e546d0cee63053a0b03848c3be6bab5c
+
+# Create your views here.
+
+def index(request):
+    news = News.objects.order_by('-published_time')
+    context = {
+        'news': news,
+    }
+    return render(request, 'index.html', context)
+
+
